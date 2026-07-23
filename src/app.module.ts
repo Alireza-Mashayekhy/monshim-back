d847a04ffff152199d2255160df03ad2876f8df3
AlirezaMashayekhy <alireza@sheydal.com>
2026-07-23 21:32:05 +0330

    fix: cookie save access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
-import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ThrottlerGuard } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { redisStore } from 'cache-manager-redis-yet';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { BarberModule } from './barber/barber.module';
+import { BookingModule } from './booking/booking.module';
 import { TypeOrmConfigService } from './common/config/typeorm.config';
 import { FilesModule } from './files/files.module';
 import { LocationsModule } from './locations/locations.module';
@@ -18,7 +17,6 @@ import { OtpModule } from './otp/otp.module';
 import { RedisModule } from './redis/redis.module';
 import { ServicesModule } from './services/services.module';
 import { UsersModule } from './users/users.module';
-import { BookingModule } from './booking/booking.module';
 
 @Module({
   imports: [
@@ -29,17 +27,17 @@ import { BookingModule } from './booking/booking.module';
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
-    CacheModule.registerAsync({
-      useFactory: async () => ({
-        store: await redisStore({
-          socket: {
-            host: process.env.REDIS_HOST || 'localhost',
-            port: parseInt(process.env.REDIS_PORT || '6381', 10) || 6379,
-          },
-          ttl: 86400, // ۱ روز (به ثانیه)
-        }),
-      }),
-    }),
+    // CacheModule.registerAsync({
+    //   useFactory: async () => ({
+    //     store: await redisStore({
+    //       socket: {
+    //         host: process.env.REDIS_HOST || 'localhost',
+    //         port: parseInt(process.env.REDIS_PORT || '6381', 10) || 6379,
+    //       },
+    //       ttl: 86400, // ۱ روز (به ثانیه)
+    //     }),
+    //   }),
+    // }),
     ThrottlerModule.forRoot([
       {
         ttl: 60000,
